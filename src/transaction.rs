3744db7565f7e92c50bc7fafb1688f46ecb6ecfc
Penn Wilbert <pwilbert@gmail.com>
2022-05-16 21:10:52 -0400

    working with some heavy mods

--- src/transaction.rs
+++ src/transaction.rs
@@ -32,7 +32,7 @@ impl Transaction {
     }
 }
 
-fn calculate_hash<T: Hash>(t: &T) -> u64 {
+pub fn calculate_hash<T: Hash>(t: &T) -> u64 {
     let mut hasher = DefaultHasher::new();
     t.hash(&mut hasher);
     hasher.finish()
--- src/transaction_search.rs
+++ src/transaction_search.rs
@@ -3,22 +3,33 @@ use crate::prelude::*;
 pub fn find_duplicates(transactions: &Vec<Transaction>) -> HashMap<u64, (Transaction, usize)>{
 
     let mut found: HashMap<u64, (Transaction, usize)> = HashMap::new();
+    let mut transaction_pos = 0;
 
-    let dup = transactions.clone();
-    for d in dup {
+    while transaction_pos < transactions.len() {
+        let transaction = &transactions[transaction_pos];
         for t in transactions {
-            if (t.uuid != d.uuid) && (t.hash == d.hash) {
-                // found.get_mut(&t.hash).unwrap().1 += 1;
-                found.entry(d.hash).or_insert((d.clone(), 0)).1 += 1;
-                // if found.contains_key(&t.hash) {
-                //     let mut f = *found.get_mut(&t.hash).unwrap();
-                //     f.1 += 1;
-                // }
-                // dbg!(&t);
-                // dbg!(&d);
+            if (t.uuid != transaction.uuid) && (t.hash == transaction.hash) {
+                found.entry(transaction.hash).or_insert((transaction.clone(), 0)).1 += 1;
             }
         }
+        transaction_pos += 1;
     }
+
+    // let dup = transactions.clone();
+    // for d in dup {
+    //     for t in transactions {
+    //         if (t.uuid != d.uuid) && (t.hash == d.hash) {
+    //             // found.get_mut(&t.hash).unwrap().1 += 1;
+    //             found.entry(d.hash).or_insert((d.clone(), 0)).1 += 1;
+    //             // if found.contains_key(&t.hash) {
+    //             //     let mut f = *found.get_mut(&t.hash).unwrap();
+    //             //     f.1 += 1;
+    //             // }
+    //             // dbg!(&t);
+    //             // dbg!(&d);
+    //         }
+    //     }
+    // }
 
     found
 }
@@ -48,8 +59,8 @@ mod tests {
         let found = find_duplicates(&transactions);
 
         for (_, values) in found {
-            if values.1 > 6 {
-                println!("Amount: {}, Count {}, Desc {}", values.0.amount, values.1, values.0.description);
+            if values.1 > 25 {
+                println!("Amount: {}, Count {:.0}, Desc {}", values.0.amount, (values.1 as f64).sqrt(), values.0.description);
             }
         }
     }
